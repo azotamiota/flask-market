c8286c96430abd4be2fe01cbbec98bb27017fb04
azotamiota <azotamiota@gmail.com>
2022-09-15 15:20:26 +0100

    add form and style register page

--- market/routes.py
+++ market/routes.py
@@ -1,6 +1,7 @@
 from market import app
 from flask import render_template
-from market.models import Item
+from market.models import Item, User
+from market.forms import RegisterForm
 
 @app.route('/')
 @app.route('/home')
@@ -12,3 +13,13 @@ def market():
     items = Item.query.all()
 
     return render_template('market.html', items=items)
+
+@app.route('/register')
+def register():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        user_to_create = User(username=form.username.data,
+                                email=form.email.data,
+                                password=form.password1.data)
+                                
+    return render_template('register.html', form=form)
