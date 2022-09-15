604236ec763fb870d211031cc833ead65a20d44c
azotamiota <azotamiota@gmail.com>
2022-09-15 11:16:14 +0100

    add a table with hardcoded items

--- market.py
+++ market.py
@@ -1,10 +1,22 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-    return 'Hello, Flask!'
+@app.route('/home')
+def home():
+    return render_template('home.html')
 
-if __name__ == '__main__':
-    app.run(debug=True)
+@app.route('/market')
+def market():
+    items = [
+        {'id': 1, 'name': 'Phone', 'barcode': '938', 'price': 500},
+        {'id': 2, 'name': 'Laptop', 'barcode': '788', 'price': 800},
+        {'id': 3, 'name': 'PC', 'barcode': '234', 'price': 1200},
+    ]
+
+    return render_template('market.html', items=items)
+
+
+# if __name__ == '__main__':
+#     app.run(debug=True)
