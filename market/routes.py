5078e0be10009f00c5bb24974793c37e5ab81c0f
azotamiota <azotamiota@gmail.com>
2022-09-15 22:36:47 +0100

    add validations

--- market/routes.py
+++ market/routes.py
@@ -1,7 +1,8 @@
 from market import app
-from flask import render_template
+from flask import render_template, redirect, url_for
 from market.models import Item, User
 from market.forms import RegisterForm
+from market import db
 
 @app.route('/')
 @app.route('/home')
@@ -14,12 +15,18 @@ def market():
 
     return render_template('market.html', items=items)
 
-@app.route('/register')
+@app.route('/register', methods=['GET', 'POST'])
 def register():
     form = RegisterForm()
     if form.validate_on_submit():
         user_to_create = User(username=form.username.data,
                                 email=form.email.data,
-                                password=form.password1.data)
-                                
+                                password_hash=form.password1.data)
+        db.session.add(user_to_create)                        
+        db.session.commit()
+        return redirect(url_for('market'))
+    if form.errors != {}:
+        for error in form.errors.values():
+            print(f'There was an error: {error}')
     return render_template('register.html', form=form)
+ 
